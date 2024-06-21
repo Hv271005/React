916372af143688e2a7a895adee7ff9a74d0ebbcc
Himanshu <hv271005@gmail.com>
2024-06-21 14:47:55 +0530

    UPDATE

--- 08minicontext/src/components/Login.jsx
+++ 08minicontext/src/components/Login.jsx
@@ -1,30 +1,28 @@
-import React, { useState, useContext} from "react"
-import userContext from "../context/UserContext"
+import React, {useState, useContext} from 'react'
+import UserContext from '../context/UserContext'
 function Login() {
-        const [username, setUsername] = useState('')
-        const [password, setPassword] = useState('')
-        
-        const setUser = useContext(useContext)
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
 
-        const handleSubmit = (e) => {
-            e.preventDefault();
-            setUser({username,password})
-        }
+    const {setUser} = useContext(UserContext)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setUser({username, password})
+    }
   return (
     <div>
         <h2>Login</h2>
-        <input type="text"
+        <input type='text'
         value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        placeholder="Username"/>
-        <br />     
-        <input type="password" 
+        onChange={(e) => setUsername(e.target.value) }
+        placeholder='username'  />
+        {" "}
+        <input type='password' 
         value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="Password"/>
-        <br />
+        onChange={(e) => setPassword(e.target.value) }
+        placeholder='password'  />
         <button onClick={handleSubmit}>Submit</button>
-        <br />
     </div>
   )
 }
--- 08minicontext/src/components/Profile.jsx
+++ 08minicontext/src/components/Profile.jsx
@@ -4,10 +4,9 @@ import UserContext from '../context/UserContext'
 function Profile() {
     const {user} = useContext(UserContext)
     
-    if (!user) return <div>PLEASE LOGIN</div>
+    if (!user) return <div>please login</div>
 
-    return <div>Welcome {user.username}</div> 
-
+    return <div>Welcome {user.username} !!!!</div>
 }
 
 export default Profile
